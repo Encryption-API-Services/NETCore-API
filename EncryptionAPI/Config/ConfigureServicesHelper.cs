8c01e6e0ab0501288089a3d01624d885255f4e42
Mike M <mtmulch0191@outlook.com>
2022-10-11 08:27:49 -0400

    Adding config helper and adding to startup class and removing Startup.

---
+++ EncryptionAPI/Config/ConfigureServicesHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace EncryptionAPI.Config
+{
+    public class ConfigureServicesHelper
+    {
+        public ConfigureServicesHelper()
+        {
+
+        }
+        public void Setup()
+        {
+            SetupDatabase();
+            SetupTransient();
+            SetupSingleton();
+            SetupScoped();
+        }
+
+        private void SetupDatabase()
+        {
+            
+
+        }
+        private void SetupTransient()
+        {
+
+        }
+        private void SetupSingleton()
+        {
+            
+        }
+        private void SetupScoped()
+        {
+            
+        }
+    }
+}
