cdad227b4b0838e1324406595cc2fec997af3f3b
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-19 03:59:32 -0400

    adding models and validation tests

---
+++ Tests/Models.Tests/UserAuthentication.cs
@@ -0,0 +1,27 @@
+﻿using Models.UserAuthentication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Models.Tests
+{
+    public class UserAuthentication
+    {
+        private string _userName = "testuseraccount";
+        private string _password = "testpassword12345";
+
+        [Fact]
+        public void CreateRegisterUserModel()
+        {
+            RegisterUser user = new RegisterUser()
+            {
+                username = this._userName,
+                password = this._password
+            };
+
+            Assert.Equal(this._userName, user.username);
+            Assert.Equal(this._password, user.password);
+        }
+    }
+}
