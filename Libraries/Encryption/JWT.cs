27ff7a7dfc89a063d3bfeebef86998258aaf7be5
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-23 21:54:30 -0400

    adding JWT class.

--- Libraries/Encryption/JWT.cs
+++ Libraries/Encryption/JWT.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Encryption
 {
     public class JWT
     {
+        public string GenerateSecurityToken(string userId, RSAParameters rsaParameters)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", userId) }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new RsaSecurityKey(rsaParameters), SecurityAlgorithms.HmacSha512Signature)
+            };
 
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+
+        }
     }
+}
 }
