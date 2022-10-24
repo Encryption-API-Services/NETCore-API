1c719bf37042959b29b06eb99dd9fbf9db68d131
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-24 14:27:55 -0400

    adding ValidateJWTToken middleware

---
+++ Libraries/Validation/Middleware/ValidateJWToken.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.Mongo;
+using DataLayer.Mongo.Entities;
+using DataLayer.Mongo.Repositories;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation.Middleware
+{
+    public class ValidateJWToken
+    {
+        private readonly RequestDelegate _next;
+        private readonly IDatabaseSettings _settings;
+
+        public ValidateJWToken(RequestDelegate next, IDatabaseSettings databaseSettings)
+        {
+            _next = next;
+            this._settings = databaseSettings;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+        }
+    }
+}
