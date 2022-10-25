c1a90d45b20ec4c5a8777cd077936ad9a6132e6a
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-24 21:35:29 -0400

    adding Encryption Controller

---
+++ UsersAPI/Controllers/EncryptionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace UsersAPI.Controllers
+{
+    public class EncryptionController : Controller
+    {
+
+        public EncryptionController()
+        {
+
+        }
+
+        [Route("EncryptAES")]
+        // GET: EncryptionController/Details/5
+        public async Task<IActionResult> EncryptAES()
+        {
+            return Ok();
+        }
+
+        [Route("DecryptAES")]
+        public async Task<IActionResult> DecryptAES()
+        {
+            return Ok();
+        }
+    }
+}
