1af94de0f2304381206bfed72edae575af5d9439
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-25 02:11:42 -0400

    #8 adding benchmark logger method

--- Common/BenchmarkMethodLogger.cs
+++ Common/BenchmarkMethodLogger.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Common
 {
     public class BenchmarkMethodLogger
     {
+        public string Method { get; set; }
+        public Guid ID { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
         public BenchmarkMethodLogger()
         {
+            this.StartTime = DateTime.UtcNow;
+            this.ID = Guid.NewGuid();
+            this.Method = new StackTrace().GetFrame(1).GetMethod().Name;
+        }
 
+        public void EndExecution()
+        {
+            this.EndTime = DateTime.UtcNow;
         }
     }
 }
