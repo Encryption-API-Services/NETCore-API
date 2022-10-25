9be4105a5ac10434e87534bc83238d9fc337d3d2
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-25 09:24:39 -0400

    checks if the strings are not null or empty.

--- UsersAPI/ControllersLogic/EncryptionControllerLogic.cs
+++ UsersAPI/ControllersLogic/EncryptionControllerLogic.cs
@@ -24,14 +24,17 @@ namespace UsersAPI.ControllersLogic
             IActionResult result = null;
             try
             {
-                using (AesManaged aes = new AesManaged())
+                if (!string.IsNullOrEmpty(body.Data) && !string.IsNullOrEmpty(body.Key) && !string.IsNullOrEmpty(body.IV))
                 {
-                    AESWrapper aesWrapper = new AESWrapper();
-                    byte[] cipherText = Convert.FromBase64String(body.Data);
-                    byte[] key = Convert.FromBase64String(body.Key);
-                    byte[] iv = Convert.FromBase64String(body.IV);
-                    string decryptedString = aesWrapper.DecryptStringFromBytes_Aes(cipherText, key, iv);
-                    result = new OkObjectResult(new { data = decryptedString });
+                    using (AesManaged aes = new AesManaged())
+                    {
+                        AESWrapper aesWrapper = new AESWrapper();
+                        byte[] cipherText = Convert.FromBase64String(body.Data);
+                        byte[] key = Convert.FromBase64String(body.Key);
+                        byte[] iv = Convert.FromBase64String(body.IV);
+                        string decryptedString = aesWrapper.DecryptStringFromBytes_Aes(cipherText, key, iv);
+                        result = new OkObjectResult(new { data = decryptedString });
+                    }
                 }
             }
             catch (Exception ex)
@@ -49,13 +52,16 @@ namespace UsersAPI.ControllersLogic
             IActionResult result = null;
             try
             {
-                using (AesManaged aes = new AesManaged())
+                if (!string.IsNullOrEmpty(body.DataToEncrypt))
                 {
-                    byte[] key = aes.Key;
-                    byte[] iv = aes.IV;
-                    AESWrapper aesWrapper = new AESWrapper();
-                    string encryptedString = Convert.ToBase64String(aesWrapper.Encrypt(body.DataToEncrypt, key, iv));
-                    result = new OkObjectResult(new { data = encryptedString, key = Convert.ToBase64String(key), iv = Convert.ToBase64String(iv) });
+                    using (AesManaged aes = new AesManaged())
+                    {
+                        byte[] key = aes.Key;
+                        byte[] iv = aes.IV;
+                        AESWrapper aesWrapper = new AESWrapper();
+                        string encryptedString = Convert.ToBase64String(aesWrapper.Encrypt(body.DataToEncrypt, key, iv));
+                        result = new OkObjectResult(new { data = encryptedString, key = Convert.ToBase64String(key), iv = Convert.ToBase64String(iv) });
+                    }
                 }
             }
             catch (Exception ex)
