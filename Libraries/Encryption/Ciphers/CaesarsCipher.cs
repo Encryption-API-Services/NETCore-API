bb13d4464078b8a1277b10b46d86d3d653d33c03
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-26 00:50:48 -0400

    adding ciphers folder

---
+++ Libraries/Encryption/Ciphers/CaesarsCipher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encryption.Ciphers
+{
+    public class CaesarsCipher
+    {
+        private string[] alaphet = new string[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        private int alaphetLength { get; set; }
+        public CaesarsCipher()
+        {
+            this.alaphetLength = alaphet.Length;
+        }
+    }
+}
