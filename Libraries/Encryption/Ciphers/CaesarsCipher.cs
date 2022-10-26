67791c2fd9fd39b987baf823fa02a070163d64f5
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-26 01:00:57 -0400

    #18 adding ciphers class

--- Libraries/Encryption/Ciphers/CaesarsCipher.cs
+++ Libraries/Encryption/Ciphers/CaesarsCipher.cs
@@ -6,11 +6,32 @@ namespace Encryption.Ciphers
 {
     public class CaesarsCipher
     {
-        private string[] alaphet = new string[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        private List<string> alaphet = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
         private int alaphetLength { get; set; }
         public CaesarsCipher()
         {
-            this.alaphetLength = alaphet.Length;
+            this.alaphetLength = alaphet.Count;
+        }
+
+        public string Encrypt(string text, int shift)
+        {
+            StringBuilder sb = new StringBuilder();
+            // loop through the text
+            //check is character is listed in the alaphet
+            return sb.ToString();
+        }
+
+        public string Decrypt(string text, int shift)
+        {
+            StringBuilder sb = new StringBuilder();
+            return sb.ToString();
+        }
+
+        private void Shift(ref string first, ref string second)
+        {
+            // create a placeholder
+            // store first in placeholder
+            // store second in first
         }
     }
 }
