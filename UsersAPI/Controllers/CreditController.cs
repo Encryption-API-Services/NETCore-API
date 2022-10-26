068ad440b34f2fec0f8c64d6a3bc76816b1fac4e
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-26 03:04:01 -0400

    #30 creating credit controller

---
+++ UsersAPI/Controllers/CreditController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditController : Controller
+    {
+        // GET: CreditController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
