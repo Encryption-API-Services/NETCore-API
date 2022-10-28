ce66c65c028453cfb21c77f6159f6259376c1252
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-10-28 14:14:01 -0400

    adjusting login user controller.

--- UsersAPI/ControllersLogic/UserLoginControllerLogic.cs
+++ UsersAPI/ControllersLogic/UserLoginControllerLogic.cs
@@ -76,27 +76,35 @@ namespace UsersAPI.ControllersLogic
             IActionResult result = null;
             try
             {
-                User activeUser = await this._userRepository.GetUserByEmailAndPassword(body.Email, body.Password);
+                User activeUser = await this._userRepository.GetUserByEmail(body.Email);
                 if (activeUser != null)
                 {
-                    // TODO: abstract the RSAParameters to another class that contains the already exported public and private keys in XML to be save in database.
-                    RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider(4096);
-                    RSAParameters rsaParams = RSAalg.ExportParameters(true);
-                    string privateKey = RSAalg.ToXmlString(true);
-                    string token = new JWT().GenerateSecurityToken(activeUser.Id, rsaParams, privateKey);
-                    JwtToken jwtToken = new JwtToken()
+                    BcryptWrapper wrapper = new BcryptWrapper();
+                    if (await wrapper.Verify(activeUser.Password, body.Password))
                     {
-                        Token = token,
-                        PrivateKey = RSAalg.ToXmlString(true),
-                        PublicKey = RSAalg.ToXmlString(false)
-                    };
-                    await this._userRepository.UpdateUsersJwtToken(activeUser, jwtToken);
-                    result = new OkObjectResult(new { token = token });
+                        // TODO: abstract the RSAParameters to another class that contains the already exported public and private keys in XML to be save in database.
+                        RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider(4096);
+                        RSAParameters rsaParams = RSAalg.ExportParameters(true);
+                        string privateKey = RSAalg.ToXmlString(true);
+                        string token = new JWT().GenerateSecurityToken(activeUser.Id, rsaParams, privateKey);
+                        JwtToken jwtToken = new JwtToken()
+                        {
+                            Token = token,
+                            PrivateKey = RSAalg.ToXmlString(true),
+                            PublicKey = RSAalg.ToXmlString(false)
+                        };
+                        await this._userRepository.UpdateUsersJwtToken(activeUser, jwtToken);
+                        result = new OkObjectResult(new { token = token });
+                    }
+                    else
+                    {
+                        result = new BadRequestObjectResult(new { error = "You entered an invalid password" });
+                    }
                 }
             }
             catch (Exception ex)
             {
-
+                result = new BadRequestObjectResult(new { error = ex.ToString() });
             }
             return result;
         }
