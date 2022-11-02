de56c62fac12399178f52e70da01d3a2db885cd9
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-01 21:13:48 -0400

    refactor to generate xml private and public keys from RSA Providers on instantiation.

---
+++ Libraries/Encryption/RSAProviderWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+
+namespace Encryption
+{
+    public sealed class RSAProviderWrapper
+    {
+        public RSACryptoServiceProvider _provider { get; set; }
+        public string _privateKey { get; set; }
+        public string _publicKey { get; set; }
+        public RSAProviderWrapper(int? keySize)
+        {
+            switch (keySize)
+            {
+                case 4096:
+                    this._provider = new RSACryptoServiceProvider(4096);
+                    return;
+                case 2048:
+                    this._provider = new RSACryptoServiceProvider(2048);
+                    return;
+                case 1024:
+                    this._provider = new RSACryptoServiceProvider(1024);
+                    return;
+                default:
+                    this._provider = new RSACryptoServiceProvider(4096);
+                    return;
+            }
+            this.SetKeyPair();
+        }
+        private void SetKeyPair()
+        {
+            this._privateKey = this._provider.ToXmlString(true);
+            this._publicKey = this._provider.ToXmlString(false);
+        }
+    }
+}
