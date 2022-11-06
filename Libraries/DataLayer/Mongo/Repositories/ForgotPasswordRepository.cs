7c9ca3485f9ebe3e4807e10e2ecf7978d23c781b
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-05 22:31:35 -0400

    #57 adding forgot password repository

--- Libraries/DataLayer/Mongo/Repositories/ForgotPasswordRepository.cs
+++ Libraries/DataLayer/Mongo/Repositories/ForgotPasswordRepository.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataLayer.Mongo.Repositories
 {
     public class ForgotPasswordRepository : IForgotPasswordRepository
     {
+        public Task<List<string>> GetLastFivePassword(string userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Libraries/DataLayer/Mongo/Repositories/IForgotPasswordRepository.cs
+++ Libraries/DataLayer/Mongo/Repositories/IForgotPasswordRepository.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataLayer.Mongo.Repositories
 {
     public interface IForgotPasswordRepository
     {
-
+        public Task<List<string>> GetLastFivePassword(string userId);
     }
 }
