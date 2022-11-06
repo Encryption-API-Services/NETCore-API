bb7f1e460664cc35015c2b67412eb6f880d11866
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-06 01:05:03 -0400

    #57 changed password collection

--- Libraries/DataLayer/Mongo/Repositories/ForgotPasswordRepository.cs
+++ Libraries/DataLayer/Mongo/Repositories/ForgotPasswordRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer.Mongo.Entities;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,13 @@ namespace DataLayer.Mongo.Repositories
 {
     public class ForgotPasswordRepository : IForgotPasswordRepository
     {
+        private readonly IMongoCollection<ForgotPasswordAttempt> _forgotPasswordAttempts;
+        public ForgotPasswordRepository(IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.Connection);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            this._forgotPasswordAttempts = database.GetCollection<ForgotPasswordAttempt>("ForgotPasswordAttempts");
+        }
         public Task<List<string>> GetLastFivePassword(string userId)
         {
             throw new NotImplementedException();
