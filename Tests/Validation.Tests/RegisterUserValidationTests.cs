e3ef085543e8b5ed183abde1c43c071375aa98da
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-09 18:25:07 -0500

    #93 is email valid

---
+++ Tests/Validation.Tests/RegisterUserValidationTests.cs
@@ -0,0 +1,27 @@
+﻿using Validation.UserRegistration;
+using Xunit;
+
+namespace Validation.Tests
+{
+    public class RegisterUserValidationTests
+    {
+       private RegisterUserValidation _validation { get; set; }
+
+        public RegisterUserValidationTests()
+        {
+            _validation = new RegisterUserValidation();
+        }
+
+        [Fact] 
+        public void EmailIsValid()
+        {
+            Assert.Equal(true, this._validation.IsEmailValid("mtmulch@gmail.com"));
+        }
+
+        [Fact]
+        public void EmailIsNotValid()
+        {
+            Assert.Equal(false, this._validation.IsEmailValid("mtmulch"));
+        }
+    }
+}
