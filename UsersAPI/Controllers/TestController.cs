667a84b629aa03bb9834b185fa44d07a104ca0b5
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-10 08:23:26 -0500

    adding test controller

---
+++ UsersAPI/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "That test worked";
+        }
+    }
+}
