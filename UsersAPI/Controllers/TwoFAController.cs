54b1a080a48b9b81d25cd9c8b30238489094b2a9
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-12 14:48:56 -0500

    creating controller logic and registering

---
+++ UsersAPI/Controllers/TwoFAController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwoFAController : ControllerBase
+    {
+        public TwoFAController()
+        {
+
+        }
+    }
+}
