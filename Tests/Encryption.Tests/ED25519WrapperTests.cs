7f80d29d44d39dec91d419f16d2c50f49d2a44db
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-16 16:07:48 -0500

    #77 test for signging data with ed25519

---
+++ Tests/Encryption.Tests/ED25519WrapperTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Encryption.Tests
+{
+    public class ED25519WrapperTests
+    {
+        private readonly ED25519Wrapper _ed25519;
+
+        public ED25519WrapperTests()
+        {
+            this._ed25519 = new ED25519Wrapper();
+        }
+
+        [Fact]
+        public async Task TestSignature()
+        {
+            string dataToSign = "Data to sign";
+            byte[] utf8DataToSign = Encoding.UTF8.GetBytes(dataToSign);
+            await this._ed25519.CreateKeyPair();
+            byte[] signedData = await this._ed25519.SignDataAsync(utf8DataToSign);
+            Assert.NotNull(signedData);
+            Assert.NotEqual(utf8DataToSign, signedData);
+        }
+    }
+}
