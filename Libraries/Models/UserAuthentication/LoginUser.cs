bb4a86c0dbd0c0cafd49178eea869a663adaea0a
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-19 20:33:35 -0500

    #130 adding user agent login info.

--- Libraries/Models/UserAuthentication/LoginUser.cs
+++ Libraries/Models/UserAuthentication/LoginUser.cs
@@ -8,5 +8,6 @@ namespace Models.UserAuthentication
     {
         public string Email { get; set; }
         public string Password { get; set; }
+        public string UserAgent { get; set; }
     }
 }
