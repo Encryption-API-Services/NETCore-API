c5c29b0b7d50ecb16b8e957b463bc398efe7ca3a
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-24 01:26:35 -0500

    #164 hash SHA

---
+++ Libraries/Encryption/RustSHAWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    public class RustSHAWrapper
+    {
+        [DllImport("performant_encryption.dll")]
+        private static extern string sha512_hash_password(string password);
+        [DllImport("performant_encryption.dll")]
+        private static extern bool sha512_verify_password(string password, string hashedPassword);
+
+        public string HashString(string stringTohash)
+        {
+            if (string.IsNullOrEmpty(stringTohash))
+            {
+                throw new Exception("Please provide a string to hash");
+            }
+            return sha512_hash_password(stringTohash);
+        }
+
+        public async Task<string> HashPasswordAsync(string stringTohash)
+        {
+            if (string.IsNullOrEmpty(stringTohash))
+            {
+                throw new Exception("Please provide a string to hash");
+            }
+            return await Task.Run(() =>
+            {
+                return sha512_hash_password(stringTohash);
+            });
+        }
+    }
+}
---
+++ Tests/Encryption.Tests/RustSHAWrapperTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Encryption.Tests
+{
+    public class RustSHAWrapperTests
+    {
+        private RustSHAWrapper _wrapper;
+        private string _testString;
+        public RustSHAWrapperTests()
+        {
+            this._wrapper = new RustSHAWrapper();
+            this._testString = "Test hash to hash";
+        }
+
+        [Fact]
+        public void HashPassword()
+        {
+            string hashed = this._wrapper.HashString(this._testString);
+            Assert.NotNull(hashed);
+            Assert.NotEmpty(hashed);
+        }
+
+        [Fact]
+        public async Task HashPasswordAsync()
+        {
+            string hashed = await this._wrapper.HashPasswordAsync(this._testString);
+            Assert.NotNull(hashed);
+            Assert.NotEmpty(hashed);
+        }
+
+        
+    }
+}
