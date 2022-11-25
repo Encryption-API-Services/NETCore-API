0a1d31d6f580c638b3d2620787f7e8020e2c0fa0
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-25 08:27:31 -0500

    #165 replacing bcrypt with scrypt

--- Libraries/DataLayer/Mongo/Repositories/UserRepository.cs
+++ Libraries/DataLayer/Mongo/Repositories/UserRepository.cs
@@ -12,21 +12,21 @@ namespace DataLayer.Mongo.Repositories
     public class UserRepository : IUserRepository
     {
         private readonly IMongoCollection<User> _userCollection;
-        private readonly BcryptWrapper _bcryptWrapper;
+        private readonly SCryptWrapper _sCrypt;
 
         public UserRepository(IDatabaseSettings databaseSettings)
         {
             var client = new MongoClient(databaseSettings.Connection);
             var database = client.GetDatabase(databaseSettings.DatabaseName);
             this._userCollection = database.GetCollection<User>("Users");
-            this._bcryptWrapper = new BcryptWrapper();
+            this._sCrypt = new SCryptWrapper();
         }
         public async Task AddUser(RegisterUser model)
         {
             await this._userCollection.InsertOneAsync(new User
             {
                 Username = model.username,
-                Password = await this._bcryptWrapper.HashPasswordAsync(model.password),
+                Password = await this._sCrypt.HashPasswordAsync(model.password),
                 Email = model.email,
                 IsActive = false,
                 Phone2FA = new Phone2FA()
