08f8c6c7373649d4998eaa6a3e34993fd4649f9f
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-25 08:19:24 -0500

    #166 exposing scrypt FFI implementation with unit tests

---
+++ Libraries/Encryption/SCryptWrapper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    public class SCryptWrapper
+    {
+        [DllImport("performant_encryption.dll")]
+        private static extern string scrypt_hash(string passToHash);
+
+        [DllImport("performant_encryption.dll")]
+        private static extern bool scrypt_verify(string password, string hash);
+        public string HashPassword(string passToHash)
+        {
+            if (string.IsNullOrEmpty(passToHash))
+            {
+                throw new Exception("Please provide a password to hash");
+            }
+            return scrypt_hash(passToHash);
+        }
+
+        public async Task<string> HashPasswordAsync(string passToHash)
+        {
+            if (string.IsNullOrEmpty(passToHash))
+            {
+                throw new Exception("Please provide a password to hash");
+            }
+            return await Task.Run(() =>
+            {
+                return scrypt_hash(passToHash);
+            });
+        }
+
+        public bool VerifyPassword(string password, string hash)
+        {
+            if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(hash))
+            {
+                throw new Exception("Please provide a password and a hash to verify");
+            }
+            return scrypt_verify(password, hash);
+        }
+
+        public async Task<bool> VerifyPasswordAsync(string password, string hash)
+        {
+            if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(hash))
+            {
+                throw new Exception("Please provide a password and a hash to verify");
+            }
+            return await Task.Run(() =>
+            {
+                return scrypt_verify(password, hash);
+            });
+        }
+    }
+}
---
+++ Tests/Encryption.Tests/SCryptWrapperTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Encryption.Tests
+{
+    public class SCryptWrapperTests
+    {
+        private readonly SCryptWrapper _scrypt;
+        private readonly string _password;
+        public SCryptWrapperTests()
+        {
+            this._scrypt = new SCryptWrapper();
+            this._password = "TestPasswordToHash";
+        }
+
+        [Fact]
+        public void HashPassword()
+        {
+            string hashedPassword = this._scrypt.HashPassword(this._password);
+            Assert.NotNull(hashedPassword);
+            Assert.NotEqual(hashedPassword, this._password);
+        }
+
+        [Fact]
+        public async Task HashPasswordAsync()
+        {
+            string hashedPassword = await this._scrypt.HashPasswordAsync(this._password);
+            Assert.NotNull(hashedPassword);
+            Assert.NotEqual(hashedPassword, this._password);
+        }
+
+        [Fact] 
+        public void VerifyPassword()
+        {
+            string hashedPassword = this._scrypt.HashPassword(this._password);
+            bool isValid = this._scrypt.VerifyPassword(this._password, hashedPassword);
+            Assert.Equal(true, isValid);
+        }
+
+        [Fact]
+        public async Task VerifyPasswordAsync()
+        {
+            string hashedPassword = this._scrypt.HashPassword(this._password);
+            bool isValid = await this._scrypt.VerifyPasswordAsync(this._password, hashedPassword);
+            Assert.Equal(true, isValid);
+        }
+    }
+}
