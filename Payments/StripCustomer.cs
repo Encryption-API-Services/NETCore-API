22a1180096d9f1492fa85163bc99406d92203b1b
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-25 11:35:22 -0500

    #175 creating payments library and adding strip library

---
+++ Payments/StripCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payments
+{
+    public class StripCustomer
+    {
+        private string _apiKey { get; set; }
+
+        public StripCustomer()
+        {
+            this._apiKey = Environment.GetEnvironmentVariable("StripApiKey");
+        }
+    }
+}
