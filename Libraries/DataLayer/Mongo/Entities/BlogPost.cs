d359e0cd8ce3993235d1b509a20bc653d7907d71
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-09 23:22:44 -0500

    Adding BlogPost model

---
+++ Libraries/DataLayer/Mongo/Entities/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+
+namespace DataLayer.Mongo.Entities
+{
+    public class BlogPost
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogBody { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
