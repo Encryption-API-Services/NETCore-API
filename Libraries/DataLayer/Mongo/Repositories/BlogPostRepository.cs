2e2159372868b56fcabdfa03cfac49786451269e
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-09 23:25:44 -0500

    Adding BlogPost repository

---
+++ Libraries/DataLayer/Mongo/Repositories/BlogPostRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer.Mongo.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Mongo.Repositories
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly IMongoCollection<BlogPost> _blogPosts;
+        public BlogPostRepository(IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.Connection);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            this._blogPosts = database.GetCollection<BlogPost>("BlogPosts");
+        }
+    }
+}
---
+++ Libraries/DataLayer/Mongo/Repositories/IBlogPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Mongo.Repositories
+{
+    public interface IBlogPostRepository
+    {
+
+    }
+}
