446460730ce69360a8252b6e042c4bc4b15008bd
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-17 16:13:22 -0500

    #177 adding token controller

---
+++ API/Controllers/TokenController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        // GET: api/<TokenController>
+        [HttpGet]
+        public IEnumerable<string> GetToken([FromQuery]string apiToken)
+        {
+            return new string[] { "value1", "value2" };
+        }       
+    }
+}
