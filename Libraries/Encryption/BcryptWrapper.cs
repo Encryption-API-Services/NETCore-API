be177865dbbc8f89311d60b9e291f9f434725bf2
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-22 21:25:06 -0500

    adjusting bcrypt wrapper to run on windows in production.

--- Libraries/Encryption/BcryptWrapper.cs
+++ Libraries/Encryption/BcryptWrapper.cs
@@ -6,62 +6,33 @@ namespace Encryption
 {
     public class BcryptWrapper
     {
-        [DllImport("libperformant_encryption.so")]
+        [DllImport("performant_encryption.dll")]
         private static extern string bcrypt_hash(string passToHash);
 
-        [DllImport("libperformant_encryption.so")]
+        [DllImport("performant_encryption.dll")]
         private static extern bool bcrypt_verify(string password, string hash);
 
         public string HashPassword(string passwordToHash)
         {
-            if (Environment.OSVersion.VersionString.Contains("Windows"))
-            {
-                return BCrypt.Net.BCrypt.HashPassword(passwordToHash);
-            }
-            else
-            {
-                return bcrypt_hash(passwordToHash);
-            }
+            return bcrypt_hash(passwordToHash);
         }
 
         public async Task<string> HashPasswordAsync(string passwordToHash)
         {
             return await Task.Run(() =>
             {
-                if (Environment.OSVersion.VersionString.Contains("Windows"))
-                {
-                    return BCrypt.Net.BCrypt.HashPassword(passwordToHash);
-                }
-                else
-                {
-                    return bcrypt_hash(passwordToHash);
-                }
+                return bcrypt_hash(passwordToHash);
             });
         }
         public async Task<bool> Verify(string hashedPassword, string unhashed)
         {
-
-            if (Environment.OSVersion.VersionString.Contains("Windows"))
-            {
-                return BCrypt.Net.BCrypt.Verify(unhashed, hashedPassword);
-            }
-            else
-            {
-                return bcrypt_verify(unhashed, hashedPassword);
-            }
+            return bcrypt_verify(unhashed, hashedPassword);
         }
         public async Task<bool> VerifyAsync(string hashedPassword, string unhashed)
         {
             return await Task.Run(() =>
             {
-                if (Environment.OSVersion.VersionString.Contains("Windows"))
-                {
-                    return BCrypt.Net.BCrypt.Verify(unhashed, hashedPassword);
-                }
-                else
-                {
-                    return bcrypt_verify(unhashed, hashedPassword);
-                }
+                return bcrypt_verify(unhashed, hashedPassword);
             });
         }
     }
