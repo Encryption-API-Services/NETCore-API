8283c248c756d8e3b727a9fe27fde50e0cec4ed1
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-23 18:37:39 -0500

    #172 adding scrypt verify

---
+++ Libraries/Models/Encryption/SCryptVerifyRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Encryption
+{
+    public class SCryptVerifyRequest
+    {
+        public string hashedPassword { get; set; }
+        public string password { get; set; }
+    }
+}
