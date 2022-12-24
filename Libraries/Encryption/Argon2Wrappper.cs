369b258946cb88d04897c885f25f3faf0c27fa14
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-24 08:00:40 -0500

    #230 used in a wrapper class and test cases for argon2

---
+++ Libraries/Encryption/Argon2Wrappper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    public class Argon2Wrappper
+    {
+        [DllImport("performant_encryption.dll")]
+        private static extern string argon2_hash(string passToHash);
+        [DllImport("performant_encryption.dll")]
+        private static extern bool argon2_verify(string hashedPassword, string passToVerify);
+        public string HashPassword(string passToHash)
+        {
+            if (string.IsNullOrEmpty(passToHash))
+            {
+                throw new Exception("You must provide a password to hash using argon2");
+            }
+            return argon2_hash(passToHash);
+        }
+
+        public async Task<string> HashPasswordAsync(string passToHash)
+        {
+            if (string.IsNullOrEmpty(passToHash))
+            {
+                throw new Exception("You must provide a password to hash using argon2");
+            }
+            return await Task.Run(() =>
+            {
+                return argon2_hash(passToHash);
+            });
+        }
+
+        public bool VerifyPassword(string hashedPasswrod, string password)
+        {
+            if (string.IsNullOrEmpty(hashedPasswrod) || string.IsNullOrEmpty(password))
+            {
+                throw new Exception("You must provide a hashed password and password to verify with argon2");
+            }
+            return argon2_verify(hashedPasswrod, password);
+        }
+        public async Task<bool> VerifyPasswordAsync(string hashedPasswrod, string password)
+        {
+            if (string.IsNullOrEmpty(hashedPasswrod) || string.IsNullOrEmpty(password))
+            {
+                throw new Exception("You must provide a hashed password and password to verify with argon2");
+            }
+            return await Task.Run(() =>
+            {
+                return argon2_verify(hashedPasswrod, password);
+            });
+        }
+    }
+}
---
+++ Tests/Encryption.Tests/Argon2WrapperTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Encryption.Tests
+{
+    public class Argon2WrapperTests
+    {
+        private Argon2Wrappper _argon2Wrapper;
+
+        public Argon2WrapperTests()
+        {
+            this._argon2Wrapper = new Argon2Wrappper();
+        }
+
+        [Fact]
+        public void HashPassword()
+        {
+            string password = "DoNotUSETHISPASS@!";
+            string hash = this._argon2Wrapper.HashPassword(password);
+            Assert.NotEqual(password, hash);
+        }
+
+        [Fact]
+        public async Task HashPasswordAsync()
+        {
+            string password = "DoNotUSETHISPASS@!";
+            string hash = await this._argon2Wrapper.HashPasswordAsync(password);
+            Assert.NotEqual(password, hash);
+        }
+
+        [Fact]
+        public void Verify()
+        {
+            string password = "TestPasswordToVerify";
+            string hash = this._argon2Wrapper.HashPassword(password);
+            bool isValid = this._argon2Wrapper.VerifyPassword(hash, password);
+            Assert.Equal(true, isValid);
+        }
+
+        [Fact]
+        public async Task VerifyAsync()
+        {
+            string password = "AsyncTestingToTheMoon!";
+            string hash = await this._argon2Wrapper.HashPasswordAsync(password);
+            bool isValid = await this._argon2Wrapper.VerifyPasswordAsync(hash, password);
+            Assert.NotEqual(false, isValid);
+        }
+    }
+}
