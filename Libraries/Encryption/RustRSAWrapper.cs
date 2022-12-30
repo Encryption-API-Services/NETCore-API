a09689720e3720cb3f0ea9e51c175839ca361073
Mike Mulchrone <mtmulch0191@outlook.com>
2022-12-30 09:55:36 -0500

    merge conflicts

--- Libraries/Encryption/RustRSAWrapper.cs
+++ Libraries/Encryption/RustRSAWrapper.cs
@@ -14,6 +14,50 @@ namespace Encryption
 
         [DllImport("performant_encryption.dll")]
         private static extern RustRsaKeyPair get_key_pair(int key_size);
+        [DllImport("performant_encryption.dll")]
+        private static extern string rsa_encrypt(string publicKey, string dataToEncrypt);
+
+        [DllImport("performant_encryption.dll")]
+        private static extern string rsa_decrypt(string privateKey, string dataToDecrypt);
+
+        public string RsaDecrypt(string privateKey, string dataToDecrypt)
+        {
+            if (string.IsNullOrEmpty(privateKey) || string.IsNullOrEmpty(dataToDecrypt))
+            {
+                throw new Exception("You need to provide a private key and data to decrypt to use RsaCrypt");
+            }
+            return rsa_decrypt(privateKey, dataToDecrypt);
+        }
+        public async Task<string> RsaDecryptAsync(string privateKey, string dataToDecrypt)
+        {
+            if (string.IsNullOrEmpty(privateKey) || string.IsNullOrEmpty(dataToDecrypt))
+            {
+                throw new Exception("You need to provide a private key and data to decrypt to use RsaCrypt");
+            }
+            return await Task.Run(() =>
+            {
+                return rsa_decrypt(privateKey, dataToDecrypt);
+            });
+        }
+        public string RsaEncrypt(string publicKey, string dataToEncrypt)
+        {
+            if (string.IsNullOrEmpty(publicKey) || string.IsNullOrEmpty(dataToEncrypt))
+            {
+                throw new Exception("You need to provide a public key and data to encrypt to use RsaEncrypt");
+            }
+            return rsa_encrypt(publicKey, dataToEncrypt);
+        }
+        public async Task<string> RsaEncryptAsync(string publicKey, string dataToEncrypt)
+        {
+            if (string.IsNullOrEmpty(publicKey) || string.IsNullOrEmpty(dataToEncrypt))
+            {
+                throw new Exception("You need to provide a public key and data to encrypt to use RsaEncrypt");
+            }
+            return await Task.Run(() =>
+            {
+                return rsa_encrypt(publicKey, dataToEncrypt);
+            });
+        }
 
         public RustRsaKeyPair GetKeyPair(int keySize)
         {
