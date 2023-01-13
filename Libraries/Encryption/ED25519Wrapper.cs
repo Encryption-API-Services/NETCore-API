b5c18514ef6368702041a4399c0676993dafe427
Mike Mulchrone <mtmulch0191@outlook.com>
2023-01-12 19:07:26 -0500

    #261 signing and verifying data.

--- Libraries/Encryption/ED25519Wrapper.cs
+++ Libraries/Encryption/ED25519Wrapper.cs
@@ -1,24 +1,72 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Encryption
 {
     public class ED25519Wrapper
-    { 
+    {
 
         [DllImport("performant_encryption.dll")]
         private static extern string get_ed25519_key_pair();
+        [DllImport("performant_encryption.dll")]
+        private static extern string sign_with_key_pair(string keyBytes, string dataToSign);
+        [DllImport("performant_encryption.dll")]
+        private static extern bool verify_with_key_pair(string keyBytes, string signature, string dataToVerify);
 
         public string GetKeyPair()
         {
             return get_ed25519_key_pair();
         }
-
         public async Task<string> GetKeyPairAsync()
         {
             return await Task.Run(() =>
             {
                 return get_ed25519_key_pair();
+            });
+        }
+        public string Sign(string keyBytes, string dataToSign)
+        {
+            if (string.IsNullOrEmpty(keyBytes))
+            {
+                throw new Exception("You need pass in the key bytes to sign data");
+            }
+            if (string.IsNullOrEmpty(dataToSign))
+            {
+                throw new Exception("You need to pass in data to sign, to sign data");
+            }
+            return sign_with_key_pair(keyBytes, dataToSign);
+        }
+        public async Task<string> SignAsync(string keyBytes, string dataToSign)
+        {
+            return await Task.Run(() =>
+            {
+                return Sign(keyBytes, dataToSign);
+            });
+        }
+
+        public bool Verify(string keyBytes, string signature, string dataToVerify)
+        {
+            if (string.IsNullOrEmpty(keyBytes))
+            {
+                throw new Exception("You need pass in the key bytes to verify data");
+            }
+            if (string.IsNullOrEmpty(signature))
+            {
+                throw new Exception("You need to pass in the signature to verify data");
+            }
+            if (string.IsNullOrEmpty(dataToVerify))
+            {
+                throw new Exception("You need to pass in data to verify, to verify data");
+            }
+
+            return verify_with_key_pair(keyBytes, signature, dataToVerify);
+        }
+        public async Task<bool> VerifyAsync(string keyBytes, string signature, string dataToVerify)
+        {
+            return await Task.Run(() =>
+            {
+                return Verify(keyBytes, signature, dataToVerify);
             });
         }
     }
--- Tests/Encryption.Tests/ED25519WrapperTests.cs
+++ Tests/Encryption.Tests/ED25519WrapperTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Xunit;
 using static Encryption.ED25519Wrapper;
 
@@ -24,6 +25,42 @@ namespace Encryption.Tests
         {
             string keyPair = await this._wrapper.GetKeyPairAsync();
             Assert.NotNull(keyPair);
+        }
+
+        [Fact]
+        public void SignData()
+        {
+            string keyPair = this._wrapper.GetKeyPair();
+            string signedData = this._wrapper.Sign(keyPair, "SignThisData");
+            Assert.NotNull(signedData);
+        }
+
+        [Fact]
+        public async Task SignDataAsync()
+        {
+            string keyPair = await this._wrapper.GetKeyPairAsync();
+            string signedData = this._wrapper.Sign(keyPair, "SignThisData");
+            Assert.NotNull(signedData);
+        }
+
+        [Fact]
+        public void Verify()
+        {
+            string keyPair = this._wrapper.GetKeyPair();
+            string dataToSign = "TestData12345";
+            string signature = this._wrapper.Sign(keyPair, dataToSign);
+            bool isValid = this._wrapper.Verify(keyPair, signature, dataToSign);
+            Assert.Equal(true, isValid);
+        }
+
+        [Fact]
+        public async Task VerifyAsync()
+        {
+            string keyPair = await this._wrapper.GetKeyPairAsync();
+            string dataToSign = "TestData12345";
+            string signature = await this._wrapper.SignAsync(keyPair, dataToSign);
+            bool isValid = await this._wrapper.VerifyAsync(keyPair, signature, dataToSign);
+            Assert.Equal(true, isValid);
         }
     }
 }
